f28131f0e7bdb9795c3caba21d3bd9a18756cc4e
Sean Bleier <sebleier@gmail.com>
2013-08-06 21:52:40 -0700

    Messing with travis script.

--- travis_run_all_tests.sh
+++ travis_run_all_tests.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
-./run_tests.py --settings=tests.hiredis_parser_settings -s ../redis/src/redis-server --redis-version=$REDIS_VERSION
-./run_tests.py --settings=tests.python_parser_settings -s ../redis/src/redis-server --redis-version=$REDIS_VERSION
-./run_tests.py --settings=tests.sockets_settings -s ../redis/src/redis-server --redis-version=$REDIS_VERSION
+./run_single_tests.py --settings=tests.settings.single.hiredis_settings -s ../redis/src/redis-server --redis-version=$REDIS_VERSION
+./run_single_tests.py --settings=tests.settings.single.python_parser -s ../redis/src/redis-server --redis-version=$REDIS_VERSION
+./run_single_tests.py --settings=tests.settings.single.sockets_settings -s ../redis/src/redis-server --redis-version=$REDIS_VERSION
+
+./run_multi_tests.py --settings=tests.settings.multiple.hiredis_settings -s ../redis/src/redis-server --redis-version=$REDIS_VERSION
+./run_multi_tests.py --settings=tests.settings.multiple.python_parser -s ../redis/src/redis-server --redis-version=$REDIS_VERSION
+./run_multi_tests.py --settings=tests.settings.multiple.sockets_settings -s ../redis/src/redis-server --redis-version=$REDIS_VERSION
